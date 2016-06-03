2bbfd6026b02342121aaa10c01cb8a86a22bc0d9
Thomas Zhang <cyote@sina.com>
2016-06-03 16:08:55 +0800

    add main page & client_list

--- CRM/forms.py
+++ CRM/forms.py
@@ -18,12 +18,30 @@ class ClientForm(forms.ModelForm):
         exclude = ['code', 'created', 'updated']
 
 
+class ClientEditForm(forms.ModelForm):
+    class Meta:
+        birthday = forms.DateField(widget=SelectDateWidget(years=range(date.today().year - 60, date.today().year)),
+                                   label='出生日期')
+
+    class Meta:
+        model = Client
+        exclude = ['code', 'created', 'updated']
+
+
 class AppointmentForm(forms.ModelForm):
     date = forms.DateField(widget=SelectDateWidget(), label='预约日期')
 
     class Meta:
         model = Appointment
         fields = ['client', 'time_slot', 'date', 'subject', 'comment']
+
+
+class AppointmentEditForm(forms.ModelForm):
+    date = forms.DateField(widget=SelectDateWidget(), label='预约日期')
+
+    class Meta:
+        model = Appointment
+        fields = ['time_slot', 'date', 'subject', 'confirm_status', 'comment']
 
 
 class RecordForm(forms.ModelForm):
